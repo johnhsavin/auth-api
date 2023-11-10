4834d93e3a0d2f7a871bcf172d9ccd8a8ce50067
John Savin <johnsavin@proton.me>
2023-11-10 12:24:44 -0500

    fixed errors

--- functions/src/middleware.js
+++ functions/src/middleware.js
@@ -4,19 +4,24 @@ import { secretKey } from "./creds.js";
 export async function isAuthenticated(req, res, next) {   // middleware takes three parameters (next)
 // first check if they have a token:
 const { authorization } = req.headers;    // authorization is the token
+
 // then check if the token is valid:
 if(!authorization) {
   res.status(401).send({ message: 'No authorization token found' });
   return;
 }
+
 // then check if the token is valid:
-jwt.verify(authorization, secretKey)
-.then(decoded => {        // valid token
+jwt.verify(authorization, secretKey, (err, decoded) => {
+  if(err) {           // not valid token
+res.status(401).send(err);
+return;
+  }
+
+  // valid token:
   req.locals = decoded;   //attach our decoded tokens to the request
+
   // if so, go on:
   next();
-})
-.catch(err => {           // not valid token
-  res.status(401).send(err);
 });
 }
