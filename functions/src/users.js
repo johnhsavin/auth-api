26446f81acfe9764746ea7ed24a8f4cdc3c547c0
John Savin <johnsavin@proton.me>
2023-11-13 12:32:56 -0500

    added bcrypt hashing of login and pw

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,4 +1,5 @@
-import { jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { db } from "./dbConnect.js";
 import { secretKey } from "./creds.js";
 
@@ -10,25 +11,25 @@ if(!email) {
   res.status(400).send({message: ' Invalid email or password.'});
   return;
 }
-await coll.add({email: email.toLowerCase(), password}); // TODO hash the password
+const hashedPw = await bcrypt.hash(password, 10); // generates salt(10) and hashes
+await coll.add({email: email.toLowerCase(), password: hashedPw }); 
 login(req, res);
 }
 
 export async function login(req, res) {
   const { email, password} = req.body;
-  const userCol = await coll.where('email', '==', email.toLowerCase())
-                             .where('password', '==', password)
-                             .get()
-
- const user = userCol.docs.map(doc => ({id: doc.id, ...doc.data() }))[0]  
- if(!user) {
-  res.status(400).send({message: ' Not authorized; missing or bad email or password'})
-  return;
- }
+  const userCol = await coll.where('email', '==', email.toLowerCase()).get();
+  const users = userCol.docs.map(doc => ({id: doc.id, ...doc.data() }));
+  const user = users.filter(user => bcrypt.compareSync(password, user.password))[0];
+  if(!user) {
+    res.status(400).send({message: ' Not authorized; missing or bad email or password'})
+    return;
+   }
+   delete user.password;    //this removes password from user object        
+   const token = jwt.sign(user, secretKey)  
+   res.send({ token })       
+  }
+  
+                             
  
- delete user.password;    //this removes password from user object        
-
- const token = jwt.sign(user, secretKey)  
- res.send({ token })       
-}
 
